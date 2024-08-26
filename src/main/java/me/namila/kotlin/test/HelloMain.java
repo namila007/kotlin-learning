497cb667cdcbbd01b9f9951e6326921ed65f86bd
Namila Bandara <namila007@gmail.com>
2024-08-27 00:25:44 +0530

    basic1

---
+++ src/main/java/me/namila/kotlin/test/HelloMain.java
@@ -0,0 +1,20 @@
+package me.namila.kotlin.test;
+
+import tute1.HelloKtKt;
+
+/**
+ * HelloMain
+ *
+ * @author - Namila
+ * @since - 8/26/2024, Mon
+ **/
+public class HelloMain
+{
+
+  public static void main(String[] args)
+  {
+    System.out.println(HelloKtKt.add(1,2));
+
+    HelloKtKt.loopPrint('x');
+  }
+}
--- src/main/kotlin/tute1/HelloKt.kt
+++ src/main/kotlin/tute1/HelloKt.kt
@@ -1,7 +1,23 @@
 package tute1
 
+fun add(a:Int,b:Int):Int = a+b;
+
+@JvmOverloads
+fun loopPrint(char: Char = '*', count:Int=5){
+    repeat(count){
+        print(char)
+    }
+    println();
+}
+
 
 fun main(args: Array<String>) {
     val name =if(args.size>0)args[0] else "kotlin";
     println("Hello $name")
+
+    val list = listOf(1,2,3,4,5);
+    println(list.joinToString(separator = "//", prefix = "(", postfix = ")"));
+
+    loopPrint('a',2)
+    loopPrint(count=4)
 }
